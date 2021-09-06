26a79c946683254d2ca7d2d6979eb16b1b235e49
alfathya <alfathya.kha01@gmail.com>
2021-09-06 14:49:18 +0700

    pembuatan home page

---
+++ list-book/src/App.js
@@ -0,0 +1,17 @@
+import Home from './components/Home';
+import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route 
+// } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
