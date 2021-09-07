23c24ddfaae185cb2bcdbcda7c50badb0be2e4e5
alfathya <alfathya.kha01@gmail.com>
2021-09-07 14:56:52 +0700

    penambahan login

--- list-book/src/App.js
+++ list-book/src/App.js
@@ -1,16 +1,23 @@
-import Home from './components/Home';
+import logo from './logo.svg';
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route 
-// } from "react-router-dom";
 
-function App() {
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisPage from './pages/RegisPage';
+
+const App = () => {
+  
+
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>  
+        <Route exact path="/login" component={LoginPage}/>
+        <Route exact path="/regis" component={RegisPage}/>
+      </Switch>
+    </Router>
+
   );
 }
 
