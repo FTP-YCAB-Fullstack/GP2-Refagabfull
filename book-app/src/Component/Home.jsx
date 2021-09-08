d9544873a8f08fa07e67d279313bcd6b99090e86
alfathya <alfathya.kha01@gmail.com>
2021-09-08 13:21:27 +0700

    test style

--- book-app/src/Component/Home.jsx
+++ book-app/src/Component/Home.jsx
@@ -31,28 +31,35 @@ const Home = () =>{
         localStorage.removeItem("status", false)
         history.push('/login')
     }
-        return(
-            <div className="w-screen h-screen flex ">
-                <div className="image-home flex justify-center items-center flex-col">
-                <button type="button" onClick={handleSignOut}>SignOut</button>
-                    <form onSubmit={handleForm}>
-                        <div class="mt-10 font-black text-6xl text-red-700">
-                            <h1>Welcome to RefaGabfull Library</h1>
-                        </div>
+    return(
+        <div className="w-screen h-screen flex ">
+            <div className="image-home ">
+                <div className="bg-blue-300 mt-5 flex justify-end mr-10">
+                <button className="" type="button" onClick={handleSignOut}>SignOut</button>
+                </div>
                 
-                        <div class="p-8 flex justify-center"> 
-                            <div class="bg-white flex items-center rounded-full shadow-xl w-4/5">
-                                <input class="rounded-l-full w-full py-4 px-6 text-yellow-800 font-bold leading-tight focus:outline-none bg-white" id="search" type="text" onChange={input} value={book} placeholder="Search the book here..."/>
-                                <div class="p-4">
-                                    <button class="bg-yellow-600 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"><i class="fas fa-search text-white"></i></button>
+                <div className="flex justify-center items-center flex-col">
+                    <div>
+                        <form onSubmit={handleForm}>
+                            <div class="mt-10 font-black text-6xl text-red-700">
+                                <h1>Welcome to RefaGabfull Library</h1>
+                            </div>
+                    
+                            <div class="p-8 flex justify-center"> 
+                                <div class="bg-white flex items-center rounded-full shadow-xl w-4/5">
+                                    <input class="rounded-l-full w-full py-4 px-6 text-yellow-800 font-bold leading-tight focus:outline-none bg-white" id="search" type="text" onChange={input} value={book} placeholder="Search the book here..."/>
+                                    <div class="p-4">
+                                        <button class="bg-yellow-600 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"><i class="fas fa-search text-white"></i></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
-                    <CardModal listBook={result}/>
-                </div>
+                        </form>
+                    </div>
+                <CardModal listBook={result}/>
             </div>
-              
+        </div>
+    </div>
+            
         )
 }
 
